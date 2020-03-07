e3f09625eefa7c8ef236bf2e368518cedd6fa33d
kovila77 <354981@mail.ru>
2020-03-07 09:40:45 +0500

    fix

---
+++ ISolver.cs
@@ -0,0 +1,8 @@
+﻿namespace Knapsack
+{
+    public interface ISolver
+    {
+        bool[] Solve(int M, int[] m, int[] c);
+        string GetName();
+    }
+}
--- Library1.fs
+++ Library1.fs
@@ -1,4 +1,19 @@
 ﻿namespace Integration_library
 
-type Class1() = 
-    member this.X = "F#"
+type Kovila77Solve() = 
+    interface Knapsack.ISolver with
+        member this.GetName() = "kovila77s solution, full search"
+        member this.Solve(M:int, m:int[], c:int[]) = 
+            let fst (a:List<bool>,_,_) = a
+            let rec _solve max (m:List<int>) (c:List<int>) (taken:List<bool>) mass costs = 
+                match m with
+                    | [] -> (taken, mass, costs)
+                    | h::t -> 
+                        let (takenIfTake, sumMassIfTake, sumCostsIfTake) = _solve max t c.Tail (taken@[true]) (mass+h) (costs+c.Head)
+                        let (takenIfNot, sumMassIfNot, sumCostsIfNot) = _solve max t c.Tail (taken@[false]) (mass) (costs)
+                        if sumMassIfTake > max && sumCostsIfTake <= sumCostsIfNot then 
+                            (takenIfNot, sumMassIfNot, sumCostsIfNot)
+                        else 
+                            (takenIfTake, sumMassIfTake, sumCostsIfTake)
+            (Array.ofList (fst (_solve M (Array.toList m) (Array.toList c) [] 0 0)))
+            
